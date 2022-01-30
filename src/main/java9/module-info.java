53e41fd45086bc99575ca53daf6505209cbe0b3f
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2022-01-30 19:55:07 +0100

    changed indentation to spaces

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,3 +1,3 @@
 module at.favre.lib.hkdf {
-	exports at.favre.lib.crypto; // FIXME split package with other libs, such as at.favre.lib:bcrypt
+    exports at.favre.lib.crypto; // FIXME split package with other libs, such as at.favre.lib:bcrypt
 }
